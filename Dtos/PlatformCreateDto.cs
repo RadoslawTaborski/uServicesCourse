20bf77ec6a322100fdb486996cd3ed6ae60ab060
Radoslaw Taborski <radoslawtaborski@gmail.com>
2021-10-24 17:35:52 +0200

    mv repository dir up

--- Dtos/PlatformCreateDto.cs
+++
@@ -1,9 +0,0 @@
-namespace PlatformService.Dtos
-{
-    public class PlatformCreateDto
-    {
-        public string Name { get; set; }
-        public string Publisher { get; set; }
-        public string Cost { get; set; }
-    }
-}
