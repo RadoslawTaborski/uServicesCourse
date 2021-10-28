de10dce742c71e7a0de6058a333d3463f6b596bd
Radosław Taborski <radoslaw.taborski@gmail.com>
2021-10-28 21:34:54 +0200

    Added logic of Command Service API

---
+++ CommandsService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,10 @@
+namespace CommandsService.Dtos
+{
+    public class PlatformReadDto
+    {
+        public int Id { get; set; }
+        public int ExternalID { get; set; }
+        public string Name { get; set; } 
+        
+    }
+}
