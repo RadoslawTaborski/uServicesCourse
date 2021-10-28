4c3c0eb2296499d6fa0cc55074ad9a3534829952
Radosław Taborski <radoslaw.taborski@gmail.com>
2021-10-28 18:40:07 +0200

    mssql

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -19,17 +19,31 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt =>
+            if(_env.IsProduction())
+            {
+                Console.WriteLine("--> Using SqlServer Db");
+                services.AddDbContext<AppDbContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            } 
+            else 
+            {
+                Console.WriteLine("--> Using InMem Db");
+                services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMem"));
+            }
+
 
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
@@ -64,10 +78,7 @@ namespace PlatformService
                 endpoints.MapControllers();
             });
 
-            if (env.IsDevelopment())
-            {
-                PrepDb.PrepPopulation(app);
-            }
+            PrepDb.PrepPopulation(app, env.IsProduction());
         }
     }
 }
